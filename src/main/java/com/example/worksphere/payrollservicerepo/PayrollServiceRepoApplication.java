9fd92d0216ff92cd609202200afbb2cb8d6495ac
sakout-reda <sakout-reda@hotmail.fr>
2024-11-17 13:22:20 +0100

    first commit

---
+++ src/main/java/com/example/worksphere/payrollservicerepo/PayrollServiceRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.worksphere.payrollservicerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollServiceRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollServiceRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worksphere/payrollservicerepo/PayrollServiceRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worksphere.payrollservicerepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollServiceRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
